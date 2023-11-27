a8765fc1c18c9c4909451c55136d86d6794ee954
sarath <sarathsarath93366@gmail.com>
2023-11-27 07:03:51 +0530

    deploy time changes 3

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ const managmentrouter= require("./Router/managementrouter/Router");
 const dataBase=require("./model/DBconnect");
 const cookieparser=require("cookie-parser");
 const bodyparser= require("body-parser")
-const {Server}=require("socket.io")
+
 const socketConnection=require("./Socketio/Socket")
+const http=require("http")
+const Server=http.createServer(app)
 
 
 
@@ -18,27 +20,52 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:true}))
-app.use(cors(
-
-
-      {
-        origin:["https://lifecarehospital.onrender.com","https://lifecare-managment.onrender.com"],
-        methods:["GET","POST","DELETE"],
-        credentials:true
-      }
 
 
 
-));
+const io = require("socket.io")(Server, {
+	cors: {
+            origin:["https://lifecarehospital.onrender.com","https://lifecare-managment.onrender.com"],
+            methods:["GET","POST","DELETE"],
+            credentials:true
+
+  } 
+          
+})
+
+
+
+
+// app.use(cors(
+
+
+//       {
+//         origin:["http://localhost:3000","http://localhost:3001"],
+//         methods:["GET","POST","DELETE"],
+//         credentials:true
+//       }
+
+
+
+// ));
 
 // "http://localhost:3000"
 // "https://lifecarehospital.onrender.com"
+// "https://lifecarehospital.onrender.com","https://lifecare-managment.onrender.com"
 
 app.use(cookieparser());
 
-const io= new Server({
-  cors:true
-})
+
+
+// const io= new Server({
+//   cors:true
+// })
+
+
+
+
+
+
 
 io.on("connection",(socket)=>{
 
@@ -51,7 +78,13 @@ io.on("connection",(socket)=>{
 
 
 
-app.use("/",userrouter);
+
+
+
+
+
+
+  app.use("/",userrouter);
 app.use("/manage",managmentrouter);
 
 dataBase.DB();
@@ -66,13 +99,18 @@ dataBase.DB();
 
 
 
-app.listen(3001,()=>{
-
-    console.log("server started");
-
-});
-
-io.listen(8000)
 
 
 
+
+// app.listen(3002,()=>{
+
+//     console.log("server started");
+
+// });
+
+// io.listen(8000)
+
+
+
+Server.listen(3002, () => console.log("server started"))
